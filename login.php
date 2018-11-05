7349dff5a780a03cdf555fca2e3fe64c4ec4206f
Fatma <fatma.elammari@gmail.com>
2018-11-05 10:49:56 +0100

    corrections php

--- login.php
+++ login.php
@@ -1,8 +1,6 @@
 <?php
 //var_dump($_POST);
 require "Model/function.php";
-$users = getUsers();
-//var_dump($users);
 
 //Vérifie si le formulaire est rempli
 if (!empty($_POST)) {
@@ -10,28 +8,23 @@ if (!empty($_POST)) {
     $_POST[$key] = htmlspecialchars("$value") ; //sécurise les saisies du formulaire
   }
 
+  $users = getUsers();
+  var_dump($users);
   foreach ($users as $key => $user) {
-    // Met en majuscule nom et password
-    // $user["name"] = mb_strtoupper($user["name"]);
-    // $user["password"] = mb_strtoupper($user["password"]);
-    //echo $user["name"]." ".$user["password"];
-
     // Test de l'authentification
     if($user["name"] === $_POST["name"] && $user["password"] === $_POST["password"]){
       session_start();
       $_SESSION["user"] = $user;
       header("Location: products.php");
       exit;
-      // echo "vous etes dejà inscrit";
-      // return;
     }
   }
-}
-else {
   header("Location: index.php?msg=Erreur d'identification");
   exit; // important : stop l'execution du script
 }
+else {
+  header("Location: index.php?msg=Veuillez saisir vos identifiants");
+  exit; // important : stop l'execution du script
+}
 
-
-//var_dump($_POST);
  ?>
